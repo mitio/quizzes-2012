0061d63e24f0f9a3f187e10c54d1e99d8975d4a8
Dimitar Dimitrov <me@ddimitrov.name>
2012-12-12 21:02:34 +0200

    Alter the default index action for quizzes

--- app/admin/quizzes.rb
+++ app/admin/quizzes.rb
@@ -1,2 +1,9 @@
 ActiveAdmin.register Quiz do
+  index do
+    selectable_column
+    id_column
+    column :name
+    column :updated_at
+    default_actions
+  end
 end
--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -1,5 +1,6 @@
 class Quiz < ActiveRecord::Base
   validates_presence_of :name
   validates_uniqueness_of :name
+
   attr_accessible :name
 end

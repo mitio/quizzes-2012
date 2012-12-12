82b06348b8ddd63a3543ad1b8c6703d90a2f669b
Dimitar Dimitrov <me@ddimitrov.name>
2012-12-12 19:33:37 +0200

    Default Rails 3.2.9 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Quizzes::Application.config.secret_token = '276e999c37e256deaba724c55d47c2a447ce08a848a2b910d0e603fd4097da6f86fa488ae3634320e3129d39a23f0088f29b7fb8d32705eefbc479f0e6d64194'

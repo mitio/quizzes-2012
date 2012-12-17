3e139bbbdce1a4ef2e85980ff1c2f92aa519f9f7
Dimitar Dimitrov <me@ddimitrov.name>
2012-12-17 20:33:58 +0200

    Simple styling for flashes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Quizzes::Application.routes.draw do
-  devise_for :users
-
   resources :quizzes do
     resources :questions
   end
   root to: 'quizzes#index'
 
-  ActiveAdmin.routes(self)
+  devise_for :users
   devise_for :admin_users, ActiveAdmin::Devise.config
+
+  ActiveAdmin.routes(self)
 end
